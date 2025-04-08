0c23a0b2f92fc649ff6af86757a677c558cf3fd3
sergiosanc16 <sergiosanchezillan@gmail.com>
2025-04-08 18:31:23 +0200

    prueba de correccion

--- src/Form/CsvUploadType.php
+++ src/Form/CsvUploadType.php
@@ -4,7 +4,10 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType; // ¡Esta es la importación correcta!
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CsvUploadType extends AbstractType
 {
