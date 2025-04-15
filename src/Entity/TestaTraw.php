c210fd5a081417c891d4c034210217405c69525d
sergiosanc16 <sergiosanchezillan@gmail.com>
2025-04-15 17:03:33 +0200

    Dump de raw antes de pesrsistirlo

--- src/Entity/TestaTraw.php
+++ src/Entity/TestaTraw.php
@@ -399,7 +399,6 @@ class TestaTraw
                             if($datosTareas[$i]['value']=='Yes'){
                                 $raw->setRelationshipMentioned(True);
                                 $raw->setGrantorRelationship($datosTareas[++$i]['value']);
-                                dump($raw);
                             } else {
                                 $raw->setRelationshipMentioned(False);
                                 $raw->setGrantorRelationship('Ninguna');
@@ -436,6 +435,8 @@ class TestaTraw
                     $raw->setSecondGrantor(FALSE);
                     $raw->setSecondGrantorName("Ningun@");
                 }
+                dump($raw);
+
                 $em->persist($raw);
             }
             if ((($i % $lote) === 0)) {
