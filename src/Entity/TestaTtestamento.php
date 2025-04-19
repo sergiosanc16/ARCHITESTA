3b9f15535a8fca0c43921a2f80a4852f7361f2b3
sergiosanc16 <sergiosanchezillan@gmail.com>
2025-04-19 21:49:48 +0200

    elimina inversedby de testamento

--- src/Entity/TestaTtestamento.php
+++ src/Entity/TestaTtestamento.php
@@ -45,7 +45,7 @@ class TestaTtestamento
     #[ORM\JoinColumn(name: "id_notario", referencedColumnName: "id")]
     private ?TestaTnotario $id_notario = null;
 
-    #[ORM\OneToOne(cascade: ['persist', 'remove'], targetEntity:'TestaTparentesco', inversedBy: 'testaTtestamentos')]
+    #[ORM\OneToOne(cascade: ['persist', 'remove'], targetEntity:'TestaTparentesco')]
     #[ORM\JoinColumn(name: "id_parentesco", referencedColumnName: "id")]
     private ?TestaTparentesco $id_parentesco = null;
 
