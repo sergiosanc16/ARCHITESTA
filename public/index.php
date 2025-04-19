ecfdd94a2fcc52bafc0a53aa54d1331d5546f0eb
sergiosanc16 <sergiosanchezillan@gmail.com>
2025-04-19 20:31:54 +0200

    Mantenimiento symfony

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+ini_set('memory_limit', '256M');
+use App\Kernel;
+
+require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+
+return function (array $context) {
+    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
+};
