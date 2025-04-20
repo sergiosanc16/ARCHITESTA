11b272103a49f1f9d41e13314c5b7bb9b04de5d1
sergiosanc16 <sergiosanchezillan@gmail.com>
2025-04-20 13:07:37 +0200

    añadido use Doctrine\Common\Collections\ArrayCollection a oficio

--- src/Entity/TestaToficio.php
+++ src/Entity/TestaToficio.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\TestaToficioRepository;
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TestaToficioRepository::class)]
