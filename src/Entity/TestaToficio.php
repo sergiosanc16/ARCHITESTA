85e8a7e748126e0189106bb181959cd2945f3569
sergiosanc16 <sergiosanchezillan@gmail.com>
2025-04-26 18:34:16 +0200

    mantenimiento de symfony

---
+++ src/Entity/TestaToficio.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TestaToficioRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TestaToficioRepository::class)]
+class TestaToficio
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $des_oficio = null;
+    
+    /** @var Collection<int, TestaTotorgante> */
+    #[ORM\OneToMany(mappedBy: 'id_oficio', targetEntity: TestaTotorgante::class)]
+    private Collection $id_otorgante;
+
+    public function __construct()
+    {
+        $this->id_otorgante = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getDesOficio(): ?string
+    {
+        return $this->des_oficio;
+    }
+
+    public function setDesOficio(string $des_oficio): static
+    {
+        $this->des_oficio = $des_oficio;
+
+        return $this;
+    }
+    /**
+     * @return Collection<int, TestaTotorgante>
+     */
+    public function getIdOtorgante(): Collection
+    {
+        return $this->id_otorgante;
+    }
+
+    public function addIdOtorgante(TestaTotorgante $idOtorgante): static
+    {
+        if (!$this->id_otorgante->contains($idOtorgante)) {
+            $this->id_otorgante->add($idOtorgante);
+        }
+
+        return $this;
+    }
+
+    public function removeIdOtorgante(TestaTotorgante $idOtorgante): static
+    {
+        $this->id_otorgante->removeElement($idOtorgante);
+
+        return $this;
+    }
+
+    public function __toString(): string
+    {
+        return $this->des_oficio;
+    }
+
+}
---
+++ src/Entity/TestaTotorgante.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TestaTotorganteRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TestaTotorganteRepository::class)]
+class TestaTotorgante
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $nombre = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $apellido1 = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $apellido2 = null;
+
+    #[ORM\ManyToOne(targetEntity: TestaToficio::class, inversedBy: 'id')]
+    #[ORM\JoinColumn(name: 'id_oficio', referencedColumnName: 'id', nullable: false)]
+    private ?TestaToficio $id_oficio = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): static
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getApellido1(): ?string
+    {
+        return $this->apellido1;
+    }
+
+    public function setApellido1(string $apellido1): static
+    {
+        $this->apellido1 = $apellido1;
+
+        return $this;
+    }
+
+    public function getApellido2(): ?string
+    {
+        return $this->apellido2;
+    }
+
+    public function setApellido2(string $apellido2): static
+    {
+        $this->apellido2 = $apellido2;
+
+        return $this;
+    }
+
+    public function getIdOficio(): ?TestaToficio
+    {
+        return $this->id_oficio;
+    }
+
+    public function setIdOficio(TestaToficio $id_oficio): static
+    {
+        $this->id_oficio = $id_oficio;
+
+        return $this;
+    }
+}
