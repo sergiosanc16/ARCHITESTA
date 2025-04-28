cf67ed08c021d93785429e1decb0c8d30facb1cf
sergiosanc16 <sergiosanchezillan@gmail.com>
2025-04-28 12:12:00 +0200

    dump de idTask para saber en que campo de campos se encuentran las anotaciones

--- src/Service/CsvUploadService.php
+++ src/Service/CsvUploadService.php
@@ -58,6 +58,7 @@ class CsvUploadService{
             $ilegible = FALSE;
             $raw->setClassificationId($campos['0']);
             $datosTareas = json_decode($campos[$idTask], true);
+            dump($idTask);
             dump($campos);
             dump($datosTareas);
 
