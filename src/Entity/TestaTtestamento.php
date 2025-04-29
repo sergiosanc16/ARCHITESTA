f58bb1f7142832f71704529b287dfd133c4d0a77
sergiosanc16 <sergiosanchezillan@gmail.com>
2025-04-29 23:58:44 +0200

    fin de merge

--- src/Entity/TestaTtestamento.php
+++ src/Entity/TestaTtestamento.php
@@ -11,76 +11,157 @@ class TestaTtestamento
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column(type: 'integer')]
+    #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(type: 'smallint')]
-    private int $anno;
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $anno = null;
 
-    #[ORM\Column(type: 'smallint')]
-    private int $mes;
+    #[ORM\Column]
+    private ?string $mes = null;
 
-    #[ORM\Column(type: 'smallint')]
-    private int $dia;
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $dia = null;
 
-    #[ORM\Column(type: 'boolean')]
-    private bool $mancomunado;
+    #[ORM\Column]
+    private ?bool $mancomunado = null;
 
-    #[ORM\Column(type: 'boolean')]
-    private bool $textoilegible;
+    #[ORM\Column]
+    private ?bool $textoilegible = null;
 
-    #[ORM\Column(type: 'integer')]
-    private int $num_protocolo;
+    #[ORM\Column]
+    private ?int $num_protocolo = null;
 
-    #[ORM\Column(type: 'integer')]
-    private int $num_folio;
+    #[ORM\Column]
+    private ?int $num_folio = null;
 
-    #[ORM\ManyToOne(targetEntity: TestaTpoblacion::class)]
-    #[ORM\JoinColumn(name: 'id_poblacion', referencedColumnName: 'id', nullable: true)]
-    private ?TestaTpoblacion $poblacion = null;
+    #[ORM\ManyToOne(targetEntity:'TestaTpoblacion')]
+    #[ORM\JoinColumn(name: "id_poblacion", referencedColumnName: "id")]
+    private ?TestaTpoblacion $id_poblacion = null;
 
-    #[ORM\ManyToOne(targetEntity: TestaTnotario::class)]
-    #[ORM\JoinColumn(name: 'id_notario', referencedColumnName: 'id', nullable: true)]
-    private ?TestaTnotario $notario = null;
+    #[ORM\ManyToOne(inversedBy: 'testaTtestamentos')]
+    #[ORM\JoinColumn(name: "id_notario", referencedColumnName: "id")]
+    private ?TestaTnotario $id_notario = null;
 
-    #[ORM\OneToOne(targetEntity: TestaTimagen::class)]
-    #[ORM\JoinColumn(name: 'id_imagen', referencedColumnName: 'id', unique: true, nullable: true)]
-    private ?TestaTimagen $imagen = null;
+    #[ORM\OneToOne(cascade: ['persist', 'remove'], targetEntity:'TestaTparentesco')]
+    #[ORM\JoinColumn(name: "id_parentesco", referencedColumnName: "id")]
+    private ?TestaTparentesco $id_parentesco = null;
 
-    #[ORM\ManyToOne(targetEntity: TestaTparentesco::class)]
-    #[ORM\JoinColumn(name: 'id_parentesco', referencedColumnName: 'id', nullable: true)]
-    private ?TestaTparentesco $parentesco = null;
+    #[ORM\OneToOne(cascade: ['persist', 'remove'], targetEntity:'TestaTimagen')]
+    #[ORM\JoinColumn(name: "id_imagen", referencedColumnName: "id")]
+    private ?TestaTimagen $id_imagen = null;
 
-    // Getters y setters...
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
 
-    public function getId(): ?int { return $this->id; }
+    public function getIdPoblacion(): ?TestaTpoblacion
+    {
+        return $this->id_poblacion;
+    }
 
-    public function getAnno(): int { return $this->anno; }
-    public function setAnno(int $anno): self { $this->anno = $anno; return $this; }
+    public function setIdPoblacion(?TestaTpoblacion $poblacion): self
+    {
+        $this->id_poblacion = $poblacion;
+        return $this;
+    }
 
-    public function getMes(): int { return $this->mes; }
-    public function setMes(int $mes): self { $this->mes = $mes; return $this; }
+    public function getAnno(): ?int
+    {
+        return $this->anno;
+    }
 
-    public function getDia(): int { return $this->dia; }
-    public function setDia(int $dia): self { $this->dia = $dia; return $this; }
+    public function setAnno(int $anno): static
+    {
+        $this->anno = $anno;
 
-    public function isMancomunado(): bool { return $this->mancomunado; }
-    public function setMancomunado(bool $mancomunado): self { $this->mancomunado = $mancomunado; return $this; }
+        return $this;
+    }
 
-    public function isTextoilegible(): bool { return $this->textoilegible; }
-    public function setTextoilegible(bool $textoilegible): self { $this->textoilegible = $textoilegible; return $this; }
+    public function getMes(): ?string
+    {
+        return $this->mes;
+    }
 
-    public function getNumProtocolo(): int { return $this->num_protocolo; }
-    public function setNumProtocolo(int $num_protocolo): self { $this->num_protocolo = $num_protocolo; return $this; }
+    public function setMes(string $mes): static
+    {
+        $this->mes = $mes;
 
-    public function getNumFolio(): int { return $this->num_folio; }
-    public function setNumFolio(int $num_folio): self { $this->num_folio = $num_folio; return $this; }
+        return $this;
+    }
 
-    public function getPoblacion(): ?TestaTpoblacion { return $this->poblacion; }
-    public function setPoblacion(?TestaTpoblacion $poblacion): self { $this->poblacion = $poblacion; return $this; }
+    public function getDia(): ?int
+    {
+        return $this->dia;
+    }
 
-    public function getNotario(): ?TestaTnotario { return $this->notario; }
-    public function setNotario(?TestaTnotario $notario): self { $this->notario = $notario; return $this; }
+    public function setDia(int $dia): static
+    {
+        $this->dia = $dia;
+
+        return $this;
+    }
+
+    public function isMancomunado(): ?bool
+    {
+        return $this->mancomunado;
+    }
+
+    public function setMancomunado(bool $mancomunado): static
+    {
+        $this->mancomunado = $mancomunado;
+
+        return $this;
+    }
+
+    public function isTextoilegible(): ?bool
+    {
+        return $this->textoilegible;
+    }
+
+    public function setTextoilegible(bool $textoilegible): static
+    {
+        $this->textoilegible = $textoilegible;
+
+        return $this;
+    }
+
+    public function getNumProtocolo(): ?int
+    {
+        return $this->num_protocolo;
+    }
+
+    public function setNumProtocolo(int $num_protocolo): static
+    {
+        $this->num_protocolo = $num_protocolo;
+
+        return $this;
+    }
+
+    public function getNumFolio(): ?int
+    {
+        return $this->num_folio;
+    }
+
+    public function setNumFolio(?int $num_folio): static
+    {
+        $this->num_folio = $num_folio;
+
+        return $this;
+    }
+
+    public function getIdNotario(): ?TestaTnotario
+    {
+        return $this->id_notario;
+    }
+
+    public function setIdNotario(?TestaTnotario $id_notario): static
+    {
+        $this->id_notario = $id_notario;
+
+        return $this;
+    }
 
     public function getIdParentesco(): ?TestaTparentesco
     {
@@ -94,12 +175,14 @@ class TestaTtestamento
         return $this;
     }
 
-    public function getParentesco(): ?TestaTparentesco { return $this->parentesco; }
-    public function setParentesco(?TestaTparentesco $parentesco): self { $this->parentesco = $parentesco; return $this; }
+    public function getIdImagen(): ?TestaTimagen
+    {
+        return $this->id_imagen;
+    }
 
-    public function __toString(){ 
-
-        // to show the name of the Category in the select 
+    public function setIdImagen(?TestaTimagen $id_imagen): static
+    {
+        $this->id_imagen = $id_imagen;
 
         return $this;
     }
