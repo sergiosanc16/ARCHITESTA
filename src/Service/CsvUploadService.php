95ec7c51b7f10b8051cf53340035959bdb0220b9
sergiosanc16 <sergiosanchezillan@gmail.com>
2025-05-01 20:02:27 +0200

    dump de error de json_decode

--- src/Service/CsvUploadService.php
+++ src/Service/CsvUploadService.php
@@ -58,10 +58,8 @@ class CsvUploadService{
             $raw = new TestaTraw();
             $raw->setClassificationId($campos['0']);
             $datosTareas = json_decode($campos[$idTask], true);
-            if($datosTareas==null){
-                echo "Longitud: ".strlen($datosTareas).PHP_EOL;
-                echo "Primeros 20 bytes: ".bin2hex(substr($datosTareas, 0, 20)).PHP_EOL;
-                echo "Últimos 20  bytes: ".bin2hex(substr($datosTareas, -20)).PHP_EOL;
+            if($datosTareas===null){
+                dump(json_last_error_msg());
             }
 
             if ($datosTareas) {
