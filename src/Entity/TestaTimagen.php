d295598337d40c190e117801ef1273dfa99e2a03
sergiosanc16 <sergiosanchezillan@gmail.com>
2025-05-03 22:28:53 +0200

    mantenimiento symfony

---
+++ src/Entity/TestaTimagen.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TestaTimagenRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TestaTimagenRepository::class)]
+class TestaTimagen
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $des_imagen = null;
+
+    function __construct(string $des_imagen) {
+        $this->des_imagen = $des_imagen;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getDesImagen(): ?string
+    {
+        return $this->des_imagen;
+    }
+
+    public function setDesImagen(string $des_imagen): static
+    {
+        $this->des_imagen = $des_imagen;
+
+        return $this;
+    }
+
+    public function __toString(){ 
+        // to show the name of the Category in the select 
+        return $this->des_imagen; 
+    } 
+}
