5dbc798926c0c50d84f0b83940048cf05084995d
sergiosanc16 <sergiosanchezillan@gmail.com>
2025-05-10 10:34:47 +0200

    añadido use de emailform

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,18 +6,20 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
+
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        ->add('email', EmailType::class, [
+            ->add('email', EmailType::class, [
             'label' => false,
             'attr' => [
                 'class' => 'form-control',
