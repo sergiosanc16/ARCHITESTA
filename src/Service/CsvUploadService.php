e930163210593c08fce083b5194a498ec695a8ae
sergiosanc16 <sergiosanchezillan@gmail.com>
2025-05-26 20:42:17 +0200

    carga 1

--- src/Service/CsvUploadService.php
+++ src/Service/CsvUploadService.php
@@ -22,14 +22,14 @@ class CsvUploadService{
         $uploadedFile = $form->get('csv_file')->getData();
         $tratamiento = file_get_contents($uploadedFile->getPathname());
 
-        // $tratamiento = preg_replace('/^"/', '', $tratamiento);
-        // $tratamiento = preg_replace('/";+;$/', '', $tratamiento);
-        // $tratamiento = preg_replace('/^((?:[^,]*,){5})([^,]*)(,)/', '$1"$2"$3', $tratamiento);
-        // //$tratamiento = preg_replace('/"{4}/', '""', $tratamiento);
-        // $tratamiento = preg_replace('/,""(\{)/', ',"$1', $tratamiento); 
-        // $tratamiento = preg_replace('/(\})""(,)/', '$1"$2', $tratamiento);
-        // $tratamiento = preg_replace('/,""(\[)/', ',"$1', $tratamiento);
-        // $tratamiento = preg_replace('/(\])""(,)/', '$1"$2', $tratamiento);
+        $tratamiento = preg_replace('/,""(\[)/', ',"$1', $tratamiento);
+        $tratamiento = preg_replace('/^"/', '', $tratamiento);
+        $tratamiento = preg_replace('/";+;$/', '', $tratamiento);
+        $tratamiento = preg_replace('/^((?:[^,]*,){5})([^,]*)(,)/', '$1"$2"$3', $tratamiento);
+        $tratamiento = preg_replace('/"{4}/', '""', $tratamiento);
+        $tratamiento = preg_replace('/,""(\{)/', ',"$1', $tratamiento); 
+        $tratamiento = preg_replace('/(\})""(,)/', '$1"$2', $tratamiento);
+        $tratamiento = preg_replace('/(\])""(,)/', '$1"$2', $tratamiento);
 
         $csvTemp = fopen('php://temp', 'r+');
         fwrite($csvTemp, $tratamiento);
@@ -284,10 +284,8 @@ class CsvUploadService{
                     $em->persist($testaOtorSeg);
                 }
                 $flush++;
-            } else{
-                dump($campos[$idTask]);
-                dump($datosTareas);
             }
+            
             if ((($flush % $lote) === 0)) {
                 $em->flush();
                 $em->clear();
