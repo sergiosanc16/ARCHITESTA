dfedf5784aa4927af0b97e00219befb5af7394b5
sergiosanc16 <sergiosanchezillan@gmail.com>
2025-05-26 21:17:33 +0200

    prueba

--- src/Service/ValidacionTestamentos.php
+++ src/Service/ValidacionTestamentos.php
@@ -33,12 +33,10 @@ class ValidacionTestamentos{
             $size = count($testamentos);
             $totalSize = 0;
             $auxSize = $size - 1;
-            dump($auxSize);
             $otorgantes=[];
             for($i = 0;$i<$size;$i++){
                 $otorgantes[$i] = $em->getRepository(TestaTtestaotorgante::class)->findBy(['testamento' => $testamentos[$i]->getId()]);
                 $totalSize += $auxSize;
-                dump($totalSize);
                 $auxSize--;
             }
             dump($totalSize);
@@ -54,10 +52,9 @@ class ValidacionTestamentos{
                 //comparar anno
                 $pAnno = 0;
                 $aux = 0;
-                for($i = 0;$i<$size;$i++){
+                for($i = 0;$i<$size-1;$i++){
                     for($j = $i;$j<$size;$j++){
                         if($validaciones[$i]->getIdTestamento()->getAnno() == $validaciones[$j]->getIdTestamento()->getAnno()){
-                            dump($aux);
                             $aux++;
                             dump($aux);
                         }
