a5c5777d2c19758fba3542519372c3848224769f
sergiosanc16 <sergiosanchezillan@gmail.com>
2025-06-22 20:18:13 +0200

    añadido libreria de JsonResponse

--- src/Controller/TestaTtestamentoController.php
+++ src/Controller/TestaTtestamentoController.php
@@ -13,6 +13,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/testa/ttestamento')]
