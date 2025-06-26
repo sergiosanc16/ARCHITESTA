50fb6c996ef0830ffa188c3ee07fca73d0de5a55
sergiosanc16 <sergiosanchezillan@gmail.com>
2025-06-26 23:41:32 +0200

    prueba de paginacion con toda la peticion

--- src/Controller/TestaTtestamentoController.php
+++ src/Controller/TestaTtestamentoController.php
@@ -120,7 +120,7 @@ final class TestaTtestamentoController extends AbstractController
     public function datatable(Request $request, TestaVtestavalidacionRepository $testaTtestamentoRepository): JsonResponse
     {
         $start = $request->query->getInt('start');
-        $length = 25;
+        $length = 3966;
 
         $testamentos = $testaTtestamentoRepository->findAjax($start, $length);
         $test_length = count($testamentos);
