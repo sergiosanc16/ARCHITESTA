432bb3e8bc571785715c07ca9cc5211530d35efa
sergiosanc16 <sergiosanchezillan@gmail.com>
2025-06-26 23:18:17 +0200

    prueba de data

--- src/Controller/TestaTtestamentoController.php
+++ src/Controller/TestaTtestamentoController.php
@@ -126,10 +126,8 @@ final class TestaTtestamentoController extends AbstractController
         $test_length = count($testamentos);
         $total       = $testaTtestamentoRepository->countTotal();
 
-        $data= [];
-        dump("esta aqui");
+        $data = [];
         foreach ($testamentos as $t) {
-            dump("entra al bucle");
             $estado = $t->getEstadoValidacion() == "M" 
                 ? '<img width="30px" src="/img/estrella.png">' 
                 : $t->getEstadoValidacion();
@@ -144,7 +142,7 @@ final class TestaTtestamentoController extends AbstractController
                 $acciones .= " <a href='/testamento/{$t->getIdTestamento()}/edit'><img width='30px' src='/img/editar.png'></a>";
             }
 
-            $rows = [
+            $data[] = [
                 '0' => $t->getIdTestamento(),
                 '1' => $estado,
                 '2' => $t->getAnno(),
@@ -161,7 +159,7 @@ final class TestaTtestamentoController extends AbstractController
                 '13' => $imagenHtml,
                 '14' => $acciones,
             ];
-            $data[] = array_values($rows);
+        }
 
             // dump($data);
 
